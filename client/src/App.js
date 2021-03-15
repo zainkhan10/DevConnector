4f21ec77d380f988b9a8aecec977cee5f3a3ee25
Zain Khan <zain.khaan147@gmail.com>
2021-03-15 18:37:09 +0500

    Created Landing. Register & Login Component Files

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Landing from "./layout/Landing";
-import Navbar from "./layout/Navbar";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route path="/" exact component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
 };
 
